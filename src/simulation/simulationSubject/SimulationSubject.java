a008fe9d05f1bf9379a8c29f935ff479c72b9922
mar-abc123 <mygarbagemailbox1@gmail.com>
2024-10-23 00:56:11 +0200

    tried to rewrite the program more generically

---
+++ src/simulation/simulationSubject/SimulationSubject.java
@@ -0,0 +1,7 @@
+package simulation.simulationSubject;
+
+public interface SimulationSubject {
+
+
+
+}
---
+++ src/simulation/simulationSubject/costs/DefaultMeasurements.java
@@ -0,0 +1,31 @@
+package simulation.simulationSubject.costs;
+
+public class DefaultMeasurements implements Measurements{
+
+    private Costs tracker;
+
+    @Override
+    public void resetTempTracker() {
+
+    }
+
+    @Override
+    public void ReadTempTrackerToData() {
+
+    }
+
+    @Override
+    public void addInitialCosts(Costs costs) {
+
+    }
+
+    @Override
+    public void addClosingCosts(Costs costs) {
+
+    }
+
+    @Override
+    public void addToTempTracker(Costs costs) {
+
+    }
+}
