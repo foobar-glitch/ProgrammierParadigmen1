ba0a1408b03a8d319915e78d8ad48b860488af9b
foobar-glitch <hamed.asami@rub.de>
2024-10-25 13:08:35 +0200

    Revert "Revert "implemented DefaultMeasurements""
    This reverts commit 801db6379b5c3220518ff4b1222d18bd06339a8b.

--- src/simulation/simulationSubject/costs/Costs.java
+++ src/simulation/simulationSubject/costs/Costs.java
@@ -1,6 +1,7 @@
 package simulation.simulationSubject.costs;
 
 import java.util.HashMap;
+import java.util.Set;
 
 public interface Costs<T> {
     public void addCost(String key, T cost);
@@ -8,4 +9,5 @@ public interface Costs<T> {
     HashMap<String, T> getCosts();
     void addCosts(Costs<T> costs);
     void subtractCosts(Costs<T> costs);
+    public Set<String> getKeySet();
 }
