5ccfa8e9881abbd1945a7b87a39ba53cea230ee1
julianschloer <e12322028@student.tuwien.ac.at>
2024-11-03 21:01:57 +0100

    Added functionality to read in Terrains (is this exciting? i'm not so sure)

--- src/UrbanElement.java
+++ src/UrbanElement.java
@@ -1,11 +1,34 @@
+/**
+ * This Interface is for every part of an urban space
+ * It includes Artifacts, Buildings, Terrains, possibly Cities and Countries
+ */
 public interface UrbanElement {
 
+    /**
+     * Demolishes the whole Urban Element
+     * @return the cost of demolishing this
+     */
     CostContainer demolishing();
+
+    /**
+     * Increases the age of this +1
+     * @return Annual cost of this element and its sub-elements
+     */
     CostContainer age();
 
+    /**
+     * @return Satisfaction of the people living/visiting in the urban element
+     */
     double satisfaction();
+
+    /**
+     * @return Age of this element
+     */
     int getAge();
 
+    /**
+     * @return Specific Architecture of this
+     */
     Architecture getArchitecture();
 
 }
